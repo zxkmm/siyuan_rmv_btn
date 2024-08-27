a34cd1edf7964448af10ebb98cdb8179d72af5dd
zxkmm <zxkmm@hotmail.com>
2024-08-28 00:11:31 +0800

    fix issue that could override new settings with old settings

--- src/index.ts
+++ src/index.ts
@@ -367,7 +367,7 @@ export default class siyuan_rmv_btn extends Plugin {
   }
 
   async onunload() {
-    await this.settingUtils.save();
+    // await this.settingUtils.save();
     // this.reloadInterface();
   }
 
