085f29d2173a7df08b00aa2af9c15c1a72bd0232
未来方舟 <zxkmm@hotmail.com>
2026-06-28 10:41:53 +0800

    https://github.com/siyuan-note/siyuan/issues/17855
    https://github.com/siyuan-note/siyuan/issues/17855

--- src/dynamic_i18n.ts
+++ src/dynamic_i18n.ts
@@ -1,7 +1,7 @@
 import { DataEntry } from "./sy_hardcoded_name_map";
 
 export const current_lang = window.siyuan.config.appearance.lang;
-export const lang_key_name_in_map = (current_lang === "zh_CN") ? "chinese_human_read_name" : "english_human_read_name";
+export const lang_key_name_in_map = (current_lang === "zh-CN") ? "chinese_human_read_name" : "english_human_read_name";
 //this fallback for others into english
 
 export function get_human_readable_name_by_identifier_from_map(identifier: string, map: Map<string, DataEntry>): string {
